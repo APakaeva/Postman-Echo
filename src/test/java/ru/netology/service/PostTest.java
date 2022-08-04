83f975deb71e0b1f326986b493203e8c634029b8
Анастасия Пакаева <staroxy63@gmail.com>
2022-08-05 00:49:18 +0300

    create PostTest - all tests passed

---
+++ src/test/java/ru/netology/service/PostTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class GetTest {
+    @Test
+    void shouldPostEURO() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency: EURO")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency: EURO"))
+        ;
+    }
+
+    @Test
+    void shouldPostRusText() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("currency: Мяу")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency: Мяу"))
+        ;
+    }
+}
